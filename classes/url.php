461868ce4c6a64395f510536722f552e52832714
creatoro <vito@ahogysikerul.hu>
2011-10-11 14:47:17 +0200

    Prepend the default to URI only if needed.

--- classes/url.php
+++ classes/url.php
@@ -14,19 +14,24 @@ class URL extends Kohana_URL {
 	 * @param   boolean  $index		Include the index_page in the URL
 	 * @param   mixed    $lang		Language key to prepend to the URI, or FALSE to not prepend a language
 	 * @return  string
+	 * @uses    Lang::$default_prepended
 	 * @uses    Request::$lang
 	 */
 	public static function site($uri = '', $protocol = NULL, $index = TRUE, $lang = TRUE)
 	{
-		if ($lang === TRUE)
+		if (Lang::$default_prepended OR Request::$lang !== Lang::$default)
 		{
-			// Prepend the current language to the URI
-			$uri = Request::$lang.'/'.ltrim($uri, '/');
-		}
-		elseif (is_string($lang))
-		{
-			// Prepend a custom language to the URI
-			$uri = $lang.'/'.ltrim($uri, '/');
+			// Prepend language to URI if it needs to be prepended or it's not the default
+			if ($lang === TRUE)
+			{
+				// Prepend the current language to the URI
+				$uri = Request::$lang.'/'.ltrim($uri, '/');
+			}
+			elseif (is_string($lang))
+			{
+				// Prepend a custom language to the URI
+				$uri = $lang.'/'.ltrim($uri, '/');
+			}
 		}
 
 		return parent::site($uri, $protocol, $index);
