46a3b57d58cdcacb4d5f8872069c9d94d79dc0a5
tailsx <davidof.chen@mail.utoronto.ca>
2016-01-27 00:53:37 -0500

    adding part2 as a module called contest.

--- modules/contest/classes/Controller/Contest.php
+++ modules/contest/classes/Controller/Contest.php
@@ -1,12 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Contest extends Controller {
 
 	public function action_index()
 	{
-		$view = View::factory('index');
-
+		$view = View::factory('newentry');
 		$this->response->body($view);
 	}
 
-} // End Welcome
+} // End Contest
---
+++ modules/contest/views/newentry.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Contest</h1>
+
+	<form>
+		First Name:<br>
+		<input type="text" name="first"><br>
+		Last Name:<br>
+		<input type="text" name="last"><br>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
