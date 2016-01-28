1c49ca9205c9cbe1fa878c5be2e972c004fb7206
tailsx <davidof.chen@mail.utoronto.ca>
2016-01-28 06:24:15 -0500

    adding validation

--- modules/contest/classes/Model/Person.php
+++ modules/contest/classes/Model/Person.php
@@ -3,15 +3,17 @@
 class Model_Person extends ORM{
 
 
-/*	public function rules(){
+	public function rules(){
 		return array(
 			'firstname' => array(
 				array('not_empty')
 			),
 			'email' => array(
-				array('not_empty')
+				array('not_empty'),
+				array('email'),
+				array(array($this, 'unique'), array('email', ':value')),
 			),
 		);
-	}*/
+	}
 
 }
