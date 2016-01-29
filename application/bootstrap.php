d9087fb46af52488db6d5b035543aba1268290a5
tailsx <davidof.chen@mail.utoronto.ca>
2016-01-28 19:59:42 -0500

    enable flexilang in bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -130,7 +130,8 @@ Kohana::modules(array(
 	 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
-	 'contest'  => MODPATH.'contest',
+	 'contest'    => MODPATH.'contest',
+	 'flexilang'  => MODPATH.'flexilang',
 	));
 
 /**
